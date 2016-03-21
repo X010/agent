213cd59537c5486be71c6d43cc5f09a541f9e4b3
x010 <jeffrey@JeffreydeMacBook-Pro.local>
2016-03-21 11:56:38 +0800

    init

---
+++ src/main/java/com/dssmp/agent/Application.java
@@ -0,0 +1,20 @@
+package com.dssmp.agent;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application
+{
+    public static void main( String[] args )
+    {
+
+    }
+}
